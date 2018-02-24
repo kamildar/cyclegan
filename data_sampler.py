a25e27556954d10938196b80b3ced1bbd9ad92a0
Ildar Kamaldinov <kamildraf@gmail.com>
2018-02-24 22:56:15 +0300

    add variables to sampler

--- data_sampler.py
+++ data_sampler.py
@@ -3,10 +3,15 @@
 
 # author: Kamaldinov Ildar
 import numpy as np
+from torch.autograd import Variable
+import torch
+
 
 def data_sampler(batchsize, sample_one, sample_two):
     ind_one = np.random.randint(
         low=0, high=sample_one.shape[0] - 1, size=batchsize)
     ind_two = np.random.randint(
         low=0, high=sample_one.shape[0] - 1, size=batchsize)
-    return sample_one[ind_one], sample_two[ind_two]
+    out_1 = Variable(torch.from_numpy(sample_one[ind_one]))
+    out_2 = Variable(torch.from_numpy(sample_two[ind_two]))
+    return out_1, out_2
