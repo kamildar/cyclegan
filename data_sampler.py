925bdb635e4c769badfe768ee176800f095f0755
Ildar Kamaldinov <kamildraf@gmail.com>
2018-02-28 16:21:30 +0300

    add cuda agnostic

--- data_sampler.py
+++ data_sampler.py
@@ -3,15 +3,19 @@
 
 # author: Kamaldinov Ildar
 import numpy as np
-from torch.autograd import Variable
 import torch
+from torch.autograd import Variable
+from utl import aduc
 
 
-def data_sampler(batchsize, sample_one, sample_two):
+def data_sampler(batchsize, sample_one, sample_two, use_gpu=None):
     ind_one = np.random.randint(
         low=0, high=sample_one.shape[0] - 1, size=batchsize)
     ind_two = np.random.randint(
         low=0, high=sample_one.shape[0] - 1, size=batchsize)
-    out_1 = Variable(torch.from_numpy(sample_one[ind_one]))
-    out_2 = Variable(torch.from_numpy(sample_two[ind_two]))
+
+    out_1 = aduc(Variable(torch.from_numpy(sample_one[ind_one]).float()),
+                 use_gpu)
+    out_2 = aduc(Variable(torch.from_numpy(sample_two[ind_two]).float()),
+                 use_gpu)
     return out_1, out_2
--- loss.py
+++ loss.py
@@ -4,38 +4,48 @@
 # mapping form A to B
 
 
-import torch.nn.functional as F
 import torch
+import torch.nn.functional as F
+from utl import aduc
+
 
 def compute_loss(gener_a, gener_b, 
                  discr_a, discr_b,
                  batch_a, batch_b,
-                 alpha):
-    # outputs    
+                 alpha,
+                 discr_loss = 'mse',
+                 # cyclic_opposite = 0,
+                 use_gpu=None,):
+    if discr_loss.lower() == 'mse':
+        discr_loss = F.mse_loss
+    elif discr_loss.lower() == 'bce':
+        discr_loss = F.binary_cross_entropy
+
+    # outputs
     discr_a_onrly = discr_a(batch_a)
     discr_b_onrly = discr_b(batch_b)
 
-    fake_a = gener_a(batch_b)
-    fake_b = gener_b(batch_a)
+    fake_a = aduc(gener_a(batch_b), use_gpu)
+    fake_b = aduc(gener_b(batch_a), use_gpu)
 
     discr_a_onfke = discr_a(fake_a)
     discr_b_onfke = discr_b(fake_b)
     
-    cyclic_a = gener_a(fake_b)
-    cyclic_b = gener_a(fake_a)
+    cyclic_a = aduc(gener_a(fake_b), use_gpu)
+    cyclic_b = aduc(gener_a(fake_a), use_gpu)
 
     # parts of discrimentators loss
-    discr_a_rly_loss = F.mse_loss(
+    discr_a_rly_loss = discr_loss(
         discr_a_onrly,
         torch.ones_like(discr_a_onrly))
-    discr_b_rly_loss = F.mse_loss(
+    discr_b_rly_loss = discr_loss(
         discr_b_onrly,
         torch.ones_like(discr_b_onrly))
 
-    discr_a_fke_loss = F.mse_loss(
+    discr_a_fke_loss = discr_loss(
         discr_a_onfke,
         torch.zeros_like(discr_a_onfke))
-    discr_b_fke_loss = F.mse_loss(
+    discr_b_fke_loss = discr_loss(
         discr_b_onfke,
         torch.zeros_like(discr_a_onfke))
 
@@ -55,7 +65,7 @@ def compute_loss(gener_a, gener_b,
     gener_b_cyc_loss = F.l1_loss(cyclic_b, batch_b)
 
     # generators loss
-    gener_a_loss = gener_a_fool + alpha * gener_a_cyc_loss
-    gener_b_loss = gener_b_fool + alpha * gener_b_cyc_loss
+    gener_a_loss = gener_a_fool + alpha * gener_a_cyc_loss #+ cyclic_opposite * gener_b_cyc_loss
+    gener_b_loss = gener_b_fool + alpha * gener_b_cyc_loss #+ cyclic_opposite * gener_a_cyc_loss
 
     return discr_a_loss, discr_b_loss, gener_a_loss, gener_b_loss
