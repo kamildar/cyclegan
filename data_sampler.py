9b9d600d441488160619a64c0cdaf700cc1890e2
Ildar Kamaldinov <kamildraf@gmail.com>
2018-03-09 20:52:04 +0300

    add arg grad of input for nth order derivatives

--- data_sampler.py
+++ data_sampler.py
@@ -5,17 +5,19 @@
 import numpy as np
 import torch
 from torch.autograd import Variable
-from utl import aduc
 
-
-def data_sampler(batchsize, sample_one, sample_two, use_gpu=None):
+def data_sampler(batchsize, sample_one, sample_two, requires_grad=False, use_gpu=None):
     ind_one = np.random.randint(
         low=0, high=sample_one.shape[0] - 1, size=batchsize)
     ind_two = np.random.randint(
         low=0, high=sample_one.shape[0] - 1, size=batchsize)
 
-    out_1 = aduc(Variable(torch.from_numpy(sample_one[ind_one]).float()),
-                 use_gpu)
-    out_2 = aduc(Variable(torch.from_numpy(sample_two[ind_two]).float()),
-                 use_gpu)
+    if use_gpu:
+        out_1 = Variable(torch.from_numpy(sample_one[ind_one]).float(),
+                         requires_grad=requires_grad).cuda()
+        out_2 = Variable(torch.from_numpy(sample_two[ind_two]).float(),
+                         requires_grad=requires_grad).cuda()
+    else:
+        out_1 = Variable(torch.from_numpy(sample_one[ind_one]).float())
+        out_2 = Variable(torch.from_numpy(sample_two[ind_two]).float())
     return out_1, out_2
