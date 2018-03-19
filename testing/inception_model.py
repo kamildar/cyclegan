305069855645324a9bde91a5f6a9cb156e4f3011
Ildar Kamaldinov <kamildraf@gmail.com>
2018-03-19 10:29:21 +0300

    test inception model

--- testing/inception_model.py
+++ testing/inception_model.py
@@ -6,7 +6,9 @@ k3same = dict(kernel_size=3, stride=1, padding=1)
 k5same = dict(kernel_size=5, stride=1, padding=2)
 k7same = dict(kernel_size=7, stride=1, padding=3)
 
+
 class InceptionSham(nn.Module):
+
     def __init__(self,
                  num_classes=10,
                  input_nc=1,
@@ -43,22 +45,21 @@ class InceptionSham(nn.Module):
         return x
 
 
-
 class InceptionX(nn.Module):
+
     def __init__(self, in_channels, pool_features, **kwargs):
         super().__init__()
         self.branch3x3 = nn.Sequential(
             BasicConv2d(in_channels, 16, **k3same),
             BasicConv2d(16, 32, **k3same)
-            )
+        )
 
         self.branch5x5 = BasicConv2d(in_channels, 16, **k5same)
 
         self.branch_pool = nn.Sequential(
             nn.AvgPool2d(**k3same),
             BasicConv2d(in_channels, pool_features, **k3same)
-            )
-
+        )
 
     def forward(self, x):
         branch3x3 = self.branch3x3(x)
@@ -70,14 +71,17 @@ class InceptionX(nn.Module):
 
 
 class InceptionZ(nn.Module):
+
     def __init__(self, in_channels):
         super().__init__()
         self.branch3x3 = nn.Sequential(
-            BasicConv2d(in_channels, 64, kernel_size=3, stride=2, padding=1),
+            BasicConv2d(in_channels, 64,
+                        kernel_size=3, stride=2, padding=1),
             BasicConv2d(64, 64, **k3same)
-            )
+        )
 
-        self.branch7x7 = BasicConv2d(in_channels, 32, kernel_size=7, stride=2, padding=3)
+        self.branch7x7 = BasicConv2d(in_channels, 32,
+                                     kernel_size=7, stride=2, padding=3)
 
     def forward(self, x):
         branch3x3 = self.branch3x3(x)
@@ -88,6 +92,7 @@ class InceptionZ(nn.Module):
 
 
 class BasicConv2d(nn.Module):
+
     def __init__(self, in_channels, out_channels, **kwargs):
         super().__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
